f9d416e30d6563675f26f83e11a46e2faa582381
weiji <weiji@qunhemail.com>
2022-08-23 19:12:18 +0800

    init

--- packages/prism/index.ts
+++ packages/prism/index.ts
@@ -1,38 +1,85 @@
-export const IdentifierReg = /=|\&|>|\,|<|>=|=|<=|!=|\*|\+|\-/;
+import Prism, { FormulaGrammar } from './test2';
+
+export const IdentifierReg = /\+|\-|\*|\//;
 export const FunctionReg = /(sum|concatenate|q|floor|ceil)(.*\)|[^\)]*)/gi;
+
+export const BracketsReg = /\(|\)/;
+
+const pairsMap = new Map([['(', ')']]);
+const inverseParisMap = new Map([[')', '(']]);
 
 // 当前字符是否括号包裹
 export const isInBrackets = (index: number, text: string) => {
-  let left = index - 1;
-  let right = index + 1;
-  while (left > 0 && right < text.length) {
-    if (text[left] === ')' || text[right] === '(') {
-      return false;
+  let i = 0;
+  const stack = [];
+  for (let i = 0; i < index; i++) {
+    const char = text[i];
+    if (pairsMap.get(char)) {
+      stack.push(char);
     }
-    left--;
-    right++;
+    const inverseChar = inverseParisMap.get(char);
+    if (inverseChar) {
+      if (stack.pop() !== inverseChar) {
+        return false;
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return false;
+  }
+  for (i = index + 1; i < text.length; i++) {
+    const char = text[i];
+    if (pairsMap.get(char)) {
+      stack.push(char);
+    }
+    const inverseChar = inverseParisMap.get(char);
+    if (inverseChar) {
+      if (stack.pop() !== inverseChar) {
+        return false;
+      }
+    }
   }
   return true;
 };
 
-console.log(isInBrackets(0, '1+2'));
+export const getSplitIndex = (text: string) => {
+  const list = [];
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    if (IdentifierReg.test(char) && !isInBrackets(i, text)) {
+      list.push(i);
+    }
+  }
+  return list;
+};
 
 // 根据没有括号包裹的运算符进行分隔:
 export const splitWithSeparate = (text: string) => {
-  for (let i = 0; i < text.length; i++) {
-    const char = text[i];
-    if (IdentifierReg.test(char)) {
-      console.log(IdentifierReg.test(char));
-    }
+  const indexList = getSplitIndex(text);
+  if (indexList.length === 0) {
+    return [text];
   }
+  const resList = [];
+  let left = 0;
+  let right = -1;
+  for (let index of indexList) {
+    right = index;
+    resList.push(text.slice(left, right));
+    resList.push(text[index]);
+    left = right;
+  }
+  resList.push(text.slice(left + 1));
+  return resList;
 };
 
 // const map = new Map<string, string>();
 
 export const parse = (text: string) => {
-  // 是否存在没有括号包裹的分隔符:
-  const textList = text.split(IdentifierReg);
-  console.log(textList);
+  const textList = splitWithSeparate(text);
+  for (let text of textList) {
+    const token = Prism.tokenize(text, FormulaGrammar);
+    console.log(token);
+  }
 };
-splitWithSeparate('1+(3+4-(1-2))');
-// parse('CONCATENATE(C1, (C2+C3/C4*C5-C6), C7,C8,C9)');
+
+parse('=CONCATENATE(C1, (C2+C3/C4*C5-C6), C7,C8,C9)');
--- packages/prism/test/parse.test.ts
+++ packages/prism/test/parse.test.ts
@@ -1,6 +1,25 @@
-import { isInBrackets } from '..';
+import { isInBrackets, getSplitIndex, splitWithSeparate } from '..';
 
 test('isInBrackets', () => {
   expect(isInBrackets(0, '1+2')).toBe(false);
   expect(isInBrackets(1, '1+2')).toBe(false);
+  expect(isInBrackets(1, '(1+2)')).toBe(true);
+  expect(isInBrackets(1, '1+2')).toBe(false);
+  expect(isInBrackets(2, '(1+2+(3+2))')).toBe(true);
+  expect(isInBrackets(2, '1+2+(3+2))')).toBe(false);
+  expect(isInBrackets(5, '1+2+(3+2)')).toBe(true);
+  expect(isInBrackets(5, '1+2+(3+2))')).toBe(false);
+});
+
+test('getSplitIndex success', () => {
+  expect(getSplitIndex('1+1+2')).toEqual([1, 3]);
+  expect(getSplitIndex('(1+1)+2')).toEqual([5]);
+  expect(getSplitIndex('((1+1)+2)')).toEqual([]);
+});
+
+test('splitWithSeparate', () => {
+  expect(splitWithSeparate('123+1')).toEqual(['123', '+', '1']);
+  expect(splitWithSeparate('(1+1)+2')).toEqual(['(1+1)', '+', '2']);
+  expect(splitWithSeparate('1+(3+4-(1-2))')).toEqual(['1', '+', '(3+4-(1-2))']);
+  expect(splitWithSeparate('11')).toEqual(['11']);
 });
