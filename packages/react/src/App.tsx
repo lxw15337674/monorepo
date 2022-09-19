dc4cfb6bbe588f5a59beb3cd66a000d4d13c3007
weiji <weiji@qunhemail.com>
2022-09-19 11:07:07 +0800

    slate

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,43 +1,59 @@
-import React, { useRef } from 'react';
-import initStore from './initStore';
-import { Container } from './styled';
-export interface IState {
-  theme: string;
-  color: string;
-}
-const initialState: IState = {
-  theme: 'dark',
-  color: 'red',
+import React, { useMemo, useState } from 'react';
+import { createEditor, Descendant } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+import { withHistory } from 'slate-history';
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: 'In addition to nodes that contain editable text, you can insert void nodes, which can also contain editable elements, inputs, or an entire other Slate editor.',
+      },
+    ],
+  },
+  {
+    type: 'button',
+    children: [{ text: 'this is button' }],
+  },
+];
+
+const withEditableVoids = (e) => {
+  const { apply } = e;
+  e.apply = (op) => {
+    apply(op);
+    console.log(e);
+  };
+  return e;
 };
 
-const a = new Promise(res => {
-  console.log(6);
-  res();
-}).then(res => {
-  console.log(1);
-  Promise.resolve().then(() => {
-    console.log(7);
-  });
-});
-a;
-console.log(3);
-setTimeout(() => {
-  console.log(4);
-});
+const Element = (props) => {
+  const { attributes, children, element } = props;
 
-const Example = () => {
-  const bc = useRef(new BroadcastChannel('AlienZHOU')).current;
-  bc.onmessage = function(e) {
-    const data = e.data;
-    const text = '[receive] ' + data.msg + ' —— tab ' + data.from;
-    console.log('[BroadcastChannel] receive message:', text);
-  };
+  switch (element.type) {
+    case 'button':
+      return <button>this is button</button>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const PlainTextExample = () => {
+  const editor = useMemo(
+    () => withEditableVoids(withHistory(withReact(createEditor()))),
+    [],
+  );
+  const [value, setValue] = useState<Descendant[]>([]);
   return (
-    <div>
-      <input />
-      <button onClick={() => bc.postMessage('hello')}>postMessage</button>
-    </div>
+    <Slate editor={editor} value={initialValue} onChange={setValue}>
+      <div style={{ border: 'black 1px solid' }}>
+        <Editable
+          renderElement={(props) => <Element {...props} />}
+          placeholder="Enter some plain text..."
+        />
+      </div>
+    </Slate>
   );
 };
 
-export default Example;
+export default PlainTextExample;
